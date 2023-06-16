f866c5676ea9549c1e69567ae85d383a852c4167
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-06-16 11:19:48 -0700

    model and apis completed

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,30 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config')
+
+const getAll = async () => {
+  const accounts = await db('accounts').select('id', 'name', 'budget');
+  return accounts;
 }
 
-const getById = id => {
-  // DO YOUR MAGIC
+const getById = async id => {
+  const account = await db('accounts').where('id', id).first();
+  return account;
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  const [newAccountId] = await db('accounts').insert(account);
+  return newAccountId;
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, account) => {
+  await db('accounts').update(account).where('id', id);
+  const updatedAccount = await getById(id);
+  return updatedAccount;
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+const deleteById = async id => {
+  const account = await getById(id);
+  await db('accounts').del().where('id', id);
+  return account;
 }
 
 module.exports = {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,57 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model');
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try {
+    const accounts = await Accounts.getAll();
+    res.status(202).json(accounts)
+  } catch (err) {
+    next(err);
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', async (req, res, next) => {
+  try {
+    const account = await Accounts.getById(req.params.id);
+    res.status(202).json(account);
+  } catch (err) {
+    next(err);
+  }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', async (req, res, next) => {
+  try {
+    const newAccountId = await Accounts.create(req.body);
+    const pullNewAccount = await Accounts.getById(newAccountId);
+    res.status(201).json(pullNewAccount)
+  } catch (err) {
+    next(err);
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', async (req, res, next) => {
+  try {
+    const updatedAccount = await Accounts.updateById(req.params.id, req.body);
+    res.status(200).json(updatedAccount);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const deletedAccount = await Accounts.deleteById(req.params.id);
+    res.status(200).json(deletedAccount);
+  } catch (err) {
+    next(err);
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    customMessage: "Something broke"
+  })
 })
 
 module.exports = router;
